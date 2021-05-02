78da98b5359b7701a5e2826cf260e93dbb5fa91b
James Pirie <131404@cloud.rangitoto.school.nz>
2021-05-03 09:37:48 +1200

    Finished and debugged the pipe moving system, began working on the pipe deleting system, so the game does not get linearly harder to run over time, but facing errors right now, will continue to debug.

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ class Bird:
                                                           fill="gold2")
         self.velocity = 0
         self.window = window
+        # self.can_jump = True
 
     def jump(self, event):
+        # if self.can_jump:
         self.velocity = 0
         for x in range(7):
             self.canvas.after(2)
@@ -19,54 +21,83 @@ class Bird:
         self.canvas.update()
 
     def collide_with_ground(self):
-        if self.canvas.coords(self.player_object)[3] > 800:
+        if self.canvas.coords(self.player_object)[3] > 800:  # Bottom wall
             self.velocity = 0
 
     def gravity(self):
+        self.collide_with_ground()
         terminal_velocity = 30
         if self.velocity > terminal_velocity:
             self.velocity = terminal_velocity
         self.canvas.move(self.player_object, 0, self.velocity)
 
     def compile_movement(self):
-        self.collide_with_ground()
         self.window.bind("<space>", self.jump)
         self.gravity()
 
 
 class Pipe:
-    def __init__(self, canvas, window, bird):
+    def __init__(self, canvas, window, list_of_pipes, bird):
         self.canvas = canvas
-        self.window = window
-        self.player = bird
         self.pipe_object_top = None
         self.pipe_object_bottom = None
+        self.window = window
+        self.list_of_pipes = list_of_pipes
+        self.player = bird
 
     def spawn(self):
-        self.pipe_object_top = self.canvas.create_rectangle(700, 810, 770, 500,
+        self.list_of_pipes.append(self)
+        x_location = random.randint(275, 750)
+        self.pipe_object_top = self.canvas.create_rectangle(810, x_location,
+                                                            880, 900,
                                                             fill="green2")
-        self.pipe_object_bottom = self.canvas.create_rectangle(700, 270, 770, 0,
+        self.pipe_object_bottom = self.canvas.create_rectangle(810,
+                                                               x_location - 270,
+                                                               880, 0,
                                                                fill="green2")
 
     def move_pipe(self):
         self.canvas.move(self.pipe_object_bottom, -2, 0)
         self.canvas.move(self.pipe_object_top, -2, 0)
 
+    def destroy_pipe(self):
+        if self.canvas.coords(self.pipe_object_top)[2] < -44:
+            self.canvas.delete(self.pipe_object_bottom, self.pipe_object_top)
+            self.list_of_pipes.pop(0)
+            return True
+        else:
+            return False
+
+    def collision(self):
+        pass
+
 
 def main():
     window = Tk()
     canvas = Canvas(window, width="800", height="800", background="sky blue")
     canvas.pack()
-    bird = Bird(canvas=canvas, window=window)
+    list_of_pipes = []
+    player = Bird(canvas=canvas, window=window)
     pipe = Pipe(canvas=canvas, window=window,
-                bird=bird)
+                list_of_pipes=list_of_pipes, bird=player)
     pipe.spawn()
-    pipe.move_pipe()
+    pipe_counter = 0
     while True:
-        canvas.after(1)
-        bird.velocity += 0.8
-        bird.compile_movement()
+        pipe_counter += 1
 
+        if pipe_counter == 200:
+            new_pipe = Pipe(canvas=canvas, window=window,
+                            list_of_pipes=list_of_pipes, bird=player)
+            new_pipe.spawn()
+            pipe_counter = 0
+
+        for x in range(len(list_of_pipes)):
+            list_of_pipes[x].move_pipe()
+            list_of_pipes[x].destroy_pipe()
+
+        canvas.after(1)
+        player.velocity += 0.8
+        player.compile_movement()
         canvas.update()
 
 
